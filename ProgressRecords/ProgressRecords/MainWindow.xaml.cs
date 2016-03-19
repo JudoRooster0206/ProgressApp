ff2270701550e80fd9ea5817490ad333999c361b
Nicholas Burton <tempestcomet@gmail.com>
2016-03-18 17:42:17 -0700

    Project In

---
+++ ProgressRecords/ProgressRecords/MainWindow.xaml.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace ProgressRecords
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    /// 
+    /*So this app will be about tracking my progress in my different hobbies. There will be a menu system for navigating through    
+      the different panels of each hobby. Each panel will have a calender that I can set goals and important events on and 3 lists: What I learned, what
+      I flaw in, and what I want to learn. At the bottom will be the goals I have set and next will be a check and an X, there purpose
+     *I don't need to explain. Another spot will be a track of the goals I have accomplished and the background*/
+    public partial class MainWindow : Window
+    {
+        ReminderClass remindInstance;
+        public MainWindow()
+        {
+            InitializeComponent();
+            DispatcherTimer timer = new DispatcherTimer();
+            timer.Interval = TimeSpan.FromSeconds(1);
+            timer.Tick += TimeContent;
+            timer.Start();
+            DispatcherTimer date = new DispatcherTimer();
+            date.Interval = TimeSpan.FromSeconds(1);
+            date.Tick += DateContent;
+            date.Start();
+            remindInstance = new ReminderClass();
+        }
+        void TimeContent(object sender, EventArgs e)
+        {
+           TimeLabel.Content = DateTime.Now.ToLongTimeString();
+        }
+        void DateContent(object sender, EventArgs e)
+        {
+            DateLabel.Content = DateTime.Now.ToLongDateString();
+            
+        }
+
+        #region Reminder
+        private void ReminderEntryAdd(object sender, RoutedEventArgs e)
+        {
+            
+        }
+
+        private void ReminderEntryDel(object sender, RoutedEventArgs e)
+        {
+           
+        }
+        #endregion
+    }
+}
---
+++ ProgressRecords/ProgressRecords/ReminderClass.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+
+namespace ProgressRecords
+{
+    public class ReminderClass
+    {
+        /*So all I need is the instructions for when you push the plus button, you add the reminder that's in the textbox and the urgency next to it
+          and if you press the minus you delete the selected reminder else you'll be asked to select it and have check mark buttons next to the text.*/
+        
+        public void Add_the_Entry(Button AddButton, Button DeleteButton, ListBox List_of_Reminders, TextBox NewEntry)
+        { 
+        if(NewEntry.Text.Length==0)
+            {
+              MessageBox.Show("Please enter an entry into the box.");
+            }
+        else
+            List_of_Reminders.Items.Add(NewEntry.Text);
+        }
+        public void Delete_the_Entry(Button AddButton, Button DeleteButton, ListBox List_of_Reminders, TextBox NewEntry)
+        {
+              List_of_Reminders.SelectedItem = null;
+            
+        }
+
+    }
+}
